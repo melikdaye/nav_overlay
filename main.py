4275f5f6fe76e5e81c625738569dfa3b2d8d58ee
Melik Ahmet Daye <melikdaye@gmail.com>
2025-11-26 23:54:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -32,12 +32,16 @@ def get_google_share_link(initial_url: str):
     chrome_options = Options()
 
     # Headless = new
-    chrome_options.add_argument("--headless=new")
+    chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--window-size=1366,768")
+    chrome_options.add_argument("--window-size=1600,1200")
     chrome_options.add_argument("--start-maximized")
+
+    chrome_options.add_argument("--disable-features=LiteMode")
+    chrome_options.add_argument("--disable-features=IsolateOrigins,SitePerProcess")
+    chrome_options.add_argument("--disable-blink-features=AutomationControlled")
 
     # Masaüstü user-agent kullan
     chrome_options.add_argument(
